46d8bc7eb511e400e30516e925e58244d0cb303d
akhmadazizmario <akhmadazizmario10@gmail.com>
2023-06-14 09:01:10 +0700

    Web Film React

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import './style/landingPage.css'
 function App() {
   return (
     <div>
-      {/* Intro section */}
+      {/* Intro sectionn */}
       <div className='myBG'>
         <NavigationBar/>
         <Intro />
       </div>
-      <div className='trending'>
+      <div className='trendingku'>
         <Trending />
       </div>
       {/* Intro section */}
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,9 +1,13 @@
-import {Navbar, Container, Nav} from "react-bootstrap"
+import {Container, Row} from "react-bootstrap"
 
 const Trending = () => {
     return (
         <div>
-        
+            <Container>
+             <Row>
+             <h1 className="mt-3">Trending</h1>
+             </Row>
+            </Container>
         </div>
     )
 }
